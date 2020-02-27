21da1ef93f57b9efe3d7c35caf716e330651f13f
Chris Jeon <yongjun.jeon96@gmail.com>
2020-02-27 13:22:32 -0600

    Add FX suite

---
+++ MainClassesSource/FXMain.java
@@ -0,0 +1,25 @@
+import com.sun.tools.javac.Main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class FXMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("skeleton.fxml"));
+        primaryStage.setTitle("Hello Mason");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
